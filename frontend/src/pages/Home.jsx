b9992a88bb2f7d19de81cb500e8f395054cfb0ca
Vis7044 <singhvishal7044@gmail.com>
2024-09-15 23:38:45 +0530

    home page responsiveness

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import UpcomingEvents from '../components/UpcomingEvents';
 const Home = () => {
   return (
     <div className="grid grid-col-1 xl:grid-cols-5 bg-[rgb(240,242,245)] md:ml-60 ml-0 ">
-       <div className='flex h-[calc(100vh-20px)] items-center justify-center xl:col-span-3 col-span-1 flex-col mt-5 gap-5 overflow-y-scroll hidescroll'>
+       <div className='flex h-[calc(100vh-20px)] xl:col-span-3 col-span-1 flex-col mt-5 gap-5 overflow-y-scroll hidescroll'>
         <Card />  
         <Card />  
         <Card />  
